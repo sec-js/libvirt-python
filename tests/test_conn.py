97ef0e0188b8d51af415b8508a843af9f1387ecb
Radoslaw Smigielski <rsmigiel@redhat.com>
2026-07-13 15:39:22 +0200

    tests: expand connection tests in test_conn.py
    Add functional tests for read-write and read-only connections using
    the test:///default driver.
    
    Signed-off-by: Radoslaw Smigielski <rsmigiel@redhat.com>

--- tests/test_conn.py
+++ tests/test_conn.py
@@ -18,6 +18,182 @@ class TestLibvirtConn(unittest.TestCase):
         self.assertEqual(type(doms[0]), libvirt.virDomain)
         self.assertEqual(doms[0].name(), "test")
 
+
+class TestLibvirtConnMetadata(unittest.TestCase):
+    """Connection metadata and lifecycle tests"""
+
+    def setUp(self):
+        self.conn = libvirt.open("test:///default")
+
+    def tearDown(self):
+        self.conn = None
+
+    def testConnGetURI(self):
+        uri = self.conn.getURI()
+
+        self.assertIsInstance(uri, str)
+        self.assertEqual(uri, "test:///default")
+
+    def testConnGetHostname(self):
+        hostname = self.conn.getHostname()
+
+        self.assertIsInstance(hostname, str)
+        self.assertGreater(len(hostname), 0)
+
+    def testConnGetLibVersion(self):
+        version = self.conn.getLibVersion()
+
+        self.assertIsInstance(version, int)
+        self.assertGreater(version, 0)
+
+    def testConnGetVersion(self):
+        """test driver reports hypervisor version 2"""
+        version = self.conn.getVersion()
+
+        self.assertIsInstance(version, int)
+        self.assertEqual(version, 2)
+
+    def testConnGetType(self):
+        """test driver reports type TEST"""
+        hv_type = self.conn.getType()
+
+        self.assertIsInstance(hv_type, str)
+        self.assertEqual(hv_type, "TEST")
+
+    def testConnIsAlive(self):
+        self.assertTrue(self.conn.isAlive())
+
+    def testConnIsEncrypted(self):
+        result = self.conn.isEncrypted()
+
+        self.assertIsInstance(result, (bool, int))
+        self.assertFalse(result)
+
+    def testConnIsSecure(self):
+        result = self.conn.isSecure()
+
+        self.assertIsInstance(result, (bool, int))
+        self.assertTrue(result)
+
+    def testConnClose(self):
+        self.assertTrue(self.conn.isAlive())
+
+        ret = self.conn.close()
+        self.assertEqual(ret, 0)
+
+        with self.assertRaises(libvirt.libvirtError):
+            self.conn.isAlive()
+
+    def testConnContextManager(self):
+        with libvirt.open("test:///default") as conn:
+            self.assertEqual(conn.getURI(), "test:///default")
+            self.assertTrue(conn.isAlive())
+
+        with self.assertRaises(libvirt.libvirtError):
+            conn.isAlive()
+
+
+class TestLibvirtConnOpenNegative(unittest.TestCase):
+    """Connection open failure for an invalid URI"""
+
+    def testOpenFails(self):
+        with self.assertRaises(libvirt.libvirtError) as ctx:
+            libvirt.open("not-a-valid-libvirt-uri")
+
+        self.assertIsNotNone(ctx.exception.get_error_message())
+
+
+class TestLibvirtConnReadOnly(unittest.TestCase):
+    """Read-only libvirt.openReadOnly() connection tests"""
+
+    DEFINE_XML = """
+        <domain type='test'>
+          <name>test-define-temp</name>
+          <uuid>12345678-1234-1234-1234-123456789abc</uuid>
+          <memory unit='KiB'>524288</memory>
+          <vcpu>1</vcpu>
+          <os>
+            <type>hvm</type>
+          </os>
+        </domain>
+        """
+
+    def setUp(self):
+        self.conn = libvirt.openReadOnly("test:///default")
+
+    def tearDown(self):
+        self.conn = None
+
+    def testOpenReadOnlySucceeds(self):
+        self.assertIsNotNone(self.conn)
+        self.assertTrue(self.conn.isAlive())
+
+    def testReadOnlyGetURI(self):
+        self.assertEqual(self.conn.getURI(), "test:///default")
+
+    def testReadOnlyGetType(self):
+        self.assertEqual(self.conn.getType(), "TEST")
+
+    def testReadOnlyListDomains(self):
+        domains = self.conn.listAllDomains()
+
+        self.assertIsInstance(domains, list)
+        self.assertGreater(len(domains), 0)
+        self.assertIsInstance(domains[0], libvirt.virDomain)
+
+    def testReadOnlyLookupAndInfo(self):
+        dom = self.conn.lookupByName("test")
+
+        self.assertEqual(dom.name(), "test")
+
+        info = dom.info()
+        self.assertIsInstance(info, (tuple, list))
+        self.assertEqual(len(info), 5)
+
+        state, _ = dom.state()
+        self.assertIsInstance(state, int)
+
+    def testReadOnlyXMLDesc(self):
+        dom = self.conn.lookupByName("test")
+        xml = dom.XMLDesc(0)
+
+        self.assertIsInstance(xml, str)
+        self.assertIn("<domain", xml)
+
+    def testReadOnlyDefineXMLDenied(self):
+        with self.assertRaises(libvirt.libvirtError) as ctx:
+            self.conn.defineXML(self.DEFINE_XML)
+
+        self.assertEqual(ctx.exception.get_error_code(),
+                         libvirt.VIR_ERR_OPERATION_DENIED)
+
+    def testReadOnlyDestroyDenied(self):
+        dom = self.conn.lookupByName("test")
+
+        with self.assertRaises(libvirt.libvirtError) as ctx:
+            dom.destroy()
+
+        self.assertEqual(ctx.exception.get_error_code(),
+                         libvirt.VIR_ERR_OPERATION_DENIED)
+
+    def testReadOnlyXMLDescSecureDenied(self):
+        dom = self.conn.lookupByName("test")
+
+        with self.assertRaises(libvirt.libvirtError) as ctx:
+            dom.XMLDesc(libvirt.VIR_DOMAIN_XML_SECURE)
+
+        self.assertEqual(ctx.exception.get_error_code(),
+                         libvirt.VIR_ERR_OPERATION_DENIED)
+
+    def testReadOnlyContextManager(self):
+        with libvirt.openReadOnly("test:///default") as conn:
+            self.assertEqual(conn.getURI(), "test:///default")
+            self.assertTrue(conn.isAlive())
+
+        with self.assertRaises(libvirt.libvirtError):
+            conn.isAlive()
+
+
 class TestLibvirtConnAuth(unittest.TestCase):
     connXML = """
 <node>
