efa45daa0a50323a32dcd1f2c4e9a765e977df70
Radoslaw Smigielski <rsmigiel@redhat.com>
2026-07-29 21:55:03 +0200

    docs: publish libvirt-python API docs via GitLab pages
    Add jobs:
     - website_job
     - pages
    to build libvirt-python API docs using Sphinx.
    
    Created base on libvirt-php/libvirt-java/libvirt-ruby examples.
    
    Signed-off-by: Radoslaw Smigielski <rsmigiel@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,41 @@ autodoc_default_options = {
 
 html_theme = 'alabaster'
 html_theme_options = {
-    'description': 'Python bindings for the libvirt virtualization API',
+    'description': 'Python bindings for the libvirt API',
     'github_button': False,
 }
+
+
+def _needs_literal_docstring(lines):
+    """Here comes a little bit of magic.
+    This function returns True if docstring content is unsafe as reStructuredText,
+    it will tell Sphinx to use the docstring as is, without any formatting.
+
+    Docs from C API descriptions are not always valid reStructuredText,
+    these are valid for Python help() but are not docutils.
+    """
+    in_field = False
+    for line in lines:
+        if line.startswith(':'):
+            in_field = True
+            continue
+        if in_field:
+            stripped = line.lstrip()
+            if stripped.startswith(('-', '*')) and line[:1].isspace():
+                return True
+            if line and not line[0].isspace() and not line.startswith(':'):
+                in_field = False
+        if line.startswith((' ', '\t')):
+            return True
+    return False
+
+
+def process_docstring(app, what, name, obj, options, lines):
+    if not lines or not _needs_literal_docstring(lines):
+        return
+    body = list(lines)
+    lines[:] = ['::', ''] + [('    ' + line if line else '') for line in body]
+
+
+def setup(app):
+    app.connect('autodoc-process-docstring', process_docstring)
